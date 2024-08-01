86ebfafca30cccd0ddb1f31b03bcb6705f21324a
soukikubokawa <terra9927sk@gmail.com>
2024-08-01 14:42:05 -0700

    first commit

---
+++ src/main/java/com/luv2code/jobportal/entity/JobSeekerSave.java
@@ -0,0 +1,69 @@
+package com.luv2code.jobportal.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(uniqueConstraints = {
+        // 同じ投稿に対して、同じ人は一度のみ保存できる
+        @UniqueConstraint(columnNames = {"userId", "job"})
+})
+public class JobSeekerSave implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // referencedColumnNameは結合先テーブル(Users, JobLocation, JobCompany)の
+    // どのカラム名を参照するかを記述する
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "jobPostId")
+    private JobPostActivity job;
+
+    public JobSeekerSave() {
+    }
+
+    public JobSeekerSave(Integer id, JobSeekerProfile userId, JobPostActivity job) {
+        this.id = id;
+        this.userId = userId;
+        this.job = job;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerSave{" +
+                "id=" + id +
+                ", userId=" + userId.toString() +
+                ", job=" + job.toString() +
+                '}';
+    }
+}
